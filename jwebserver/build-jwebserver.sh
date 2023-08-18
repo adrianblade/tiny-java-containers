aba8f4e77ccf451da07d8b00a92ebf45ef680892
Adrian Catalan <adrian.catalan@mango.com>
2023-08-18 08:46:39 +0200

    add jwebserver sources

--- jwebserver/build-jwebserver.sh
+++ jwebserver/build-jwebserver.sh
@@ -1,13 +1,25 @@
 #!/bin/sh
 
-native-image --static --libc=musl -m jdk.httpserver -o jwebserver.static
-docker build . -f Dockerfile.static -t jwebserver:static
-rm -f jwebserver.upx
-upx --lzma --best -o jwebserver.upx jwebserver.static 
-docker build . -f Dockerfile.upx -t jwebserver:upx
+docker build -f Dockerfile.jvm -t jwebserver:jvm  .
+docker run --rm -d --name jwebserver.jvm jwebserver.jvm:latest
+sleep 1
+curl http://172.17.0.2:8000/index.html
+curl http://172.17.0.2:8000/get_simple_1.json
+docker stop jwebserver.jvm
 
-echo "Generated Executables"
-ls -lh jwebserver.static jwebserver.upx
+docker build -f Dockerfile.static -t jwebserver:static .
+docker run --rm -d --name jwebserver.static jwebserver.static:latest
+sleep 1
+curl http://172.17.0.2:8000/index.html
+curl http://172.17.0.2:8000/get_simple_1.json
+docker stop jwebserver.static
+
+docker build -f Dockerfile.upx -t jwebserver:upx .
+docker run --rm -d --name jwebserver.upx jwebserver.upx:latest
+sleep 1
+curl http://172.17.0.2:8000/index.html
+curl http://172.17.0.2:8000/get_simple_1.json
+docker stop jwebserver.upx
 
 echo "Generated Docker Container Images"
 docker images jwebserver
