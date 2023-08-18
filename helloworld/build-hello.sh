78a64fba786036ba06963b779a9c09e6e26d7b15
Adrian Catalan <adrian.catalan@mango.com>
2023-08-18 08:48:12 +0200

    add jvm helloworld

--- helloworld/build-hello.sh
+++ helloworld/build-hello.sh
@@ -2,6 +2,8 @@
 
 echo "Generating docker images ..."
 
+docker build . -f Dockerfile.jvm -t hello:jvm
+
 docker build . -f Dockerfile.static -t hello:static
 
 docker build . -f Dockerfile.upx -t hello:upx
@@ -10,9 +12,12 @@ echo "Listing ..."
 docker images | grep "hello"
 
 echo "Executing ..."
+docker run -it hello:jvm
 docker run -it hello:static
 docker run -it hello:upx
 
+echo "Time hello:jvm: ..."
+time docker run -it hello:jvm
 echo "Time hello:static: ..."
 time docker run -it hello:static
 echo "Time hello:upx: ..."
